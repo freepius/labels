d09ccc64a054fde8d2e4afb82ab15ebac82ca293
freepius <contact@freepius.net>
2025-08-13 11:48:59 +0200

    Add a toolbox to rotate the label

--- assets/app.js
+++ assets/app.js
@@ -20,7 +20,41 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = url.toString();
     }
 
+    /**
+     * Manage the toolbox on label page.
+     */
+    function manageToolbox() {
+        const rotateLeftButton = document.getElementById('turn-left');
+        const rotateRightButton = document.getElementById('turn-right');
+        const resetButton = document.getElementById('reset');
+
+        const rotationStep = 90;
+        const availableRotations = Array.from({length: 360 / rotationStep}, (_, i) => i * rotationStep);
+
+        function removeRotationClasses() {
+            const label = document.querySelector('.label');
+            availableRotations.forEach(deg => label.classList.remove(`rotate-${deg}`));
+        }
+
+        // Set the new rotation class.
+        function rotate(factor) {
+            const label = document.querySelector('.label');
+            const has = deg => label.classList.contains(`rotate-${deg}`);
+            const current = availableRotations.find(deg => has(deg)) || 0;
+            const newRotation = (current + factor * rotationStep + 360) % 360;
+
+            removeRotationClasses();
+            label.classList.add(`rotate-${newRotation}`);
+        }
+
+        rotateLeftButton.addEventListener('click', () => rotate(-1));
+        rotateRightButton.addEventListener('click', () => rotate(1));
+        resetButton.addEventListener('click', removeRotationClasses);
+    }
+
     document.querySelectorAll('input[name="version"]').forEach(input => {
         input.addEventListener('change', updateVersionQuery);
     });
+
+    manageToolbox();
 });
